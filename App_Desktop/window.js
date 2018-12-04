a5bb2e976c3224fa048137f49542834648245106
OceaneCsn <oceane.cassan@insa-lyon.fr>
2018-12-04 18:06:32 +0100

    sun trajectory

--- App_Desktop/window.js
+++ App_Desktop/window.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const chart = require('./chart.js');
+const sun = require('./drawSun.js');
 
 let i = 0;
-
+let angle = 0;
 function updatePhotoValuesChart() {
 
 
@@ -11,11 +12,16 @@ function updatePhotoValuesChart() {
     let data = [Math.random()*200,Math.random()*200, Math.random()*200] 
     chart.update(data);
     setTimeout(updatePhotoValuesChart, 1000);
+    
+    angle = (angle+5)%180
+    
+    sun.sunTraj(angle)
 
 }
 
 $(() => {
     chart.drawChart([50, 300, 200]);
     updatePhotoValuesChart();
+    
 })
 
