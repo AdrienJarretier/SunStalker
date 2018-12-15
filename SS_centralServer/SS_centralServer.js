b80c3f4636a45df71d7b7fc9e92471462303db25
Hugo Castaneda <hugo.castaneda@etu.univ-lyon1.fr>
2018-12-15 21:39:07 +0100

    updated SS_centralServer for WoT management + added a centralServer tester script

--- SS_centralServer/SS_centralServer.js
+++ SS_centralServer/SS_centralServer.js
@@ -11,8 +11,8 @@ app.use(bodyParser.json());
 
 // ----------------------------------------------------------
 
-var dataMap = {}
-var objectMap = {}
+var appObjects = {}
+var objectsMap = {}
 
 // -------------------------------
 
@@ -25,25 +25,155 @@ function generateToken() {
 function getFullOrient() {
 
   let orients = []
-  for(let token in dataMap) {
-    orients.push(dataMap[token])
+  for(let token in appObjects) {
+    if(objectData[token].Sensor != null)
+      orients.push(objectsMap[objectData[token].Sensor].photoCellValues)
   }
 
   return orients
 
 }
 
+function getAppSpace(appToken) {
+
+  if(!appObjects.hasOwnProperty(appToken))
+    appObjects[appToken] = {'Heliot':null,'Sensor':null}
+  return appObjects[appToken]
+}
+
+function getObjectSpace(objid) {
+
+  if(!objectsMap.hasOwnProperty(objid))
+    objectsMap[objid] = {'object':null,'data':null}
+  return objectsMap[objid]
+}
+
+function setObject(appToken, objType, object) {
+  let appSpace = getAppSpace(appToken)
+  let objectSpace = getObjectSpace(object.id)
+
+  appSpace[objType] = object.id
+  objectSpace.object = object
+}
+
+function removeObject(appToken, objType) {
+  let appSpace = getAppSpace(appToken)
+  if(appSpace[objType] != null) {
+    let old_object_id = appSpace[objType]
+    appSpace[objType] = null
+    delete objectsMap[old_object_id]
+  }
+}
+
+function setData(appToken, objType, data) {
+  let appSpace = getAppSpace(appToken)
+
+  if(appSpace[objType] == null)
+    return false
+
+  let objectSpace = getObjectSpace(appSpace[objType])
+  objectSpace.data = data
+  return true
+}
+
+function createRes(res,code, message, useHtml = false) {
+  res.status(code);
+  if(useHtml === true)
+    message = '<h1><b><u>Code: '+code+'</u></b>: '+message+'</h1>'
+  else
+    message = code+' - '+message+'\n\r'
+  console.log(message)
+  res.send(message);
+  return res
+}
+
+function createJSONresponse(res,jsonData) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(jsonData));
+  return res
+}
 // ----------------------------------------------------------
 
-app.post("/setMyOrientation", (req, res, next) => {
+app.post("/setMyData", (req, res, next) => {
   
-  let token = req.body.token;
-  let orientData = req.body.orientData;
+  let appToken = req.body.token;
+  let objectType = req.body.objectType;
+  let objectData = req.body.objectData;
 
-  console.log(token,'set its orientation to',orientData)
+  console.log(appToken,'trys to set its data for object',objectType,objectData)
 
-  dataMap[token] = orientData
+  if(!setData(appToken, objectType, objectData))
+    return createRes(res,401, 'error while updating data, object is not uploaded')
 
+  return createRes(res,200, 'data updated')
+
+});
+
+app.post("/setMyObject", (req, res, next) => {
+  
+  let appToken = req.body.token;
+  let objectType = req.body.objectType;
+  let object = req.body.object;
+
+  console.log(appToken,'set its representation for object',objectType)
+
+  if(object == null) {
+    removeObject(appToken, objectType)
+    return createRes(res,200, 'object removed')
+  }
+  else {
+    setObject(appToken, objectType, object)
+    return createRes(res,200, 'object updated')
+  }
+
+});
+
+// ----------------------
+
+app.get("/getObjects/:apptoken", (req, res, next) => {
+  
+  let apptoken = req.params.apptoken;
+
+  console.log('someone trys to get all objects from',apptoken)
+
+  if(!appObjects.hasOwnProperty(apptoken)) {
+    return createRes(res,401, 'apptoken cannot be found')
+  }
+
+  let heliotId = appObjects[apptoken].Heliot
+  let sensorId = appObjects[apptoken].Sensor
+
+  let objs = {}
+
+  if(heliotId == null)
+    objs['Heliot'] = null
+  else
+    objs['Heliot'] = objectsMap[heliotId]
+
+  if(sensorId == null)
+    objs['Sensor'] = null
+  else
+    objs['Sensor'] = objectsMap[sensorId]
+
+  return createJSONresponse(res,objs)
+
+});
+
+app.get("/objects/:objid/:property", (req, res, next) => {
+  
+  let objid = req.params.objid;
+  let property = req.params.property;
+
+  console.log('someone trys to get data',property,'from object',objid)
+
+  if(!objectsMap.hasOwnProperty(objid)) {
+    return createRes(res,401, 'object cannot be found')
+  }
+  if(objectsMap[objid].data == null || !objectsMap[objid].data.hasOwnProperty(property)) {
+    return createRes(res,401, 'object has no property '+property)
+  }
+
+  return createJSONresponse(res,objectsMap[objid].data[property])
 });
 
 // ----------------------
---
+++ SS_centralServer/centralServerTester.js
@@ -0,0 +1,142 @@
+'use strict'
+
+const common = require('../common.js')
+const request = require('request-promise-native');
+
+// ------------------------------------------------------
+
+let SunStalkerServerUrl = 'http://localhost:' + common.centralServerConfig.port
+let myToken = null
+
+async function getToken() {
+
+  if(myToken != null) {
+    return myToken
+  }
+
+  let options = {
+    uri: SunStalkerServerUrl + '/requireToken',
+    method: 'GET',
+  }
+
+  try {
+    myToken = await request(options)
+  }
+  catch(error) {
+    return null
+  }
+
+  return myToken
+}
+// -----------------------------
+async function sendData(objectType, objectData) {
+
+  let token = await getToken()
+
+  let options = {
+    uri: SunStalkerServerUrl+'/setMyData',
+    method: 'POST',
+    json: true,
+    body: {'token':token,'objectType':objectType,'objectData':objectData}
+  }
+
+  try {
+    return await request(options)
+  }
+  catch(error) {
+    return null
+  }
+}
+// -----------------------------
+async function sendObject(type,object) {
+
+  let token = await getToken()
+
+  let options = {
+    uri: SunStalkerServerUrl+'/setMyObject',
+    method: 'POST',
+    json: true,
+    body: {'token': token, 'objectType': type, 'object':object}
+  }
+
+  try {
+    return await request(options)
+  }
+  catch(error) {
+    return null
+  }
+}
+// -----------------------------
+async function getObjectData(objid,property) {
+
+  let options = {
+    uri: SunStalkerServerUrl+'/objects/' + objid + '/' +property,
+    method: 'GET',
+    json: true,
+  }
+
+  try {
+    return await request(options)
+  }
+  catch(error) {
+    return null
+  }
+}
+// -----------------------------
+async function retrieveAllObject(token) {
+
+  let options = {
+    uri: SunStalkerServerUrl+'/getObjects/' + token,
+    method: 'GET',
+    json: true,
+  }
+
+  try {
+    return await request(options)
+  }
+  catch(error) {
+    return null
+  }
+}
+
+// -------------------------------------------------------
+
+async function main() {
+  let resp = null
+
+  // ----- add objects
+  resp = await sendObject('Heliot',{'id':'heliotid','desc':'je suis un heliot'})
+  console.log(resp)
+  resp = await retrieveAllObject(await getToken())
+  console.log(resp)
+  resp = await sendObject('Sensor',{'id':'sensorid','desc':'je suis un sensor'})
+  console.log(resp)
+  resp = await retrieveAllObject(await getToken())
+  console.log(resp)
+
+
+  // ----- set object data
+  resp = await sendData('Heliot',{'myData':[0,1,2,3]})
+  console.log(resp)
+  resp = await getObjectData('heliotid','myData')
+  console.log(resp)
+  resp = await getObjectData('sensorid','myData')
+  console.log(resp)
+
+  // ----- remove objects
+  resp = await sendObject('Heliot',null)
+  console.log(resp)
+  resp = await sendData('Heliot',{'myData':[0,1,2,3]})
+  console.log(resp)
+  resp = await getObjectData('heliotid','myData')
+  console.log(resp)
+
+  // ----- retrive orientations
+  resp = await sendData('Sensor',{'myData':[0,1,2,3]})
+  console.log(resp)
+}
+
+console.log('trying ...')
+main().then(function(){
+  console.log('done')
+})
