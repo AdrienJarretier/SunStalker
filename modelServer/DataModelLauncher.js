713cc2391252c61de6c395b84f017297c409e2e6
Hugo Castaneda <hugodecasta@orange.fr>
2018-12-15 19:36:36 +0100

    Created a dataModel simple terminal launcher

---
+++ modelServer/DataModelLauncher.js
@@ -0,0 +1,19 @@
+const dataModel = require('./dataModel.js')
+
+console.log('data model launched !')
+
+let serial = dataModel.getSerialInterface()
+
+serial.bindToSensorConnect(function(){
+  console.log('New Sensor connected')
+})
+serial.bindToSensorDisconnect(function(){
+  console.log('Sensor disconnected')
+})
+
+serial.bindToHeliotConnect(function(){
+  console.log('New Heliot connected')
+})
+serial.bindToHeliotDisconnect(function(){
+  console.log('Heliot disconnected')
+})
