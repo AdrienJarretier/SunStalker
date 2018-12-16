08a1935226aedd841e80b6965f53d5b59c86b752
Jarretier Adrien <jarretier.adrien@gmail.com>
2018-12-17 00:09:44 +0100

    fixed bar chart height

--- App_Desktop/chart.js
+++ App_Desktop/chart.js
@@ -9,19 +9,19 @@ let data;
 let y;
 let x;
 
+let width = common.WINDOW.WIDTH / 2;
+let height = width / ((1 + Math.sqrt(5)) / 2);
+
 
 function drawChart(data) {
 
     data = data;
-    let width = common.WINDOW.WIDTH / 2;
-    let height = width / ((1 + Math.sqrt(5)) / 2);
-
     let svg = d3.select('#photoValuesChart').append('svg')
         .attr("width", width)
         .attr("height", height);
 
     x = d3.scaleLinear().range([0, width]);
-    y = d3.scaleLinear().range([0, height]);
+    y = d3.scaleLinear().range([0, height-20]);
 
     x.domain([0, data.length]);
     y.domain([0, 1024]);
@@ -45,7 +45,7 @@ function drawChart(data) {
     let names = ["WEST", "ZENITH", "EAST"]
     g.selectAll('text').data(data).enter().append('text')
         .attr("x", function (d, i) { return i * width / 3 + width / 12 })
-        .attr("y", height - 100)
+        .attr("y", height)
         .text(function (d, i) {
             return names[i];
         });
@@ -57,7 +57,7 @@ function update(data) {
     data = data;
     g.selectAll('rect').data(data).transition()
         .attr('y', function (d) {
-            return 200 - y(d);
+            return height-20 - y(d);
         })
         .attr('height', function (d) { return y(d); })
 }
